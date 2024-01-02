98e327bc80341adc1648db77f1ed49eb1592b3ce
Roger Kennerly Nascimento <roger-kn@hotmail.com>
2024-01-02 09:57:09 -0300

    Add Nodemon

--- src/server.js
+++ src/server.js
@@ -2,5 +2,7 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (request, response) => {});
+
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
