2aab2313f04feb7d85365f09e8933ff8495fbaa0
Roger Kennerly Nascimento <roger-kn@hotmail.com>
2024-01-02 11:58:20 -0300

    Handling errors and migrations

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,8 +1,29 @@
+const { hash } = require("bcryptjs");
+const AppError = require("../utils/AppError");
+
+const sqliteConnection = require("../database/sqlite");
+
 class UsersController {
-  create(request, response) {
+  async create(request, response) {
     const { name, email, password } = request.body;
 
-    response.status(201).json({ name, email, password });
+    const database = await sqliteConnection();
+
+    const checkUserExists = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    );
+
+    if (checkUserExists) {
+      throw new AppError("Este e-mail já está em uso.");
+    }
+
+    await database.run(
+      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+      [name, email, password]
+    );
+
+    return response.status(201).json();
   }
 }
 
