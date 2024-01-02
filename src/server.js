0a06e5d25b7c3dcda3645537321b01f20f39cc27
Roger Kennerly Nascimento <roger-kn@hotmail.com>
2024-01-02 11:02:35 -0300

    Separation of responsibilities

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
-const app = express();
+const routes = require("./routes");
 
-app.get("/", (request, response) => {
-  response.send("hi");
-});
+const app = express();
+app.use(express.json());
+
+app.use(routes);
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
